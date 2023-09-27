485afc8c302f32687ba889277f761531e6264cbc
Guigo1008 <guigo@eq.ufrj.br>
2023-09-26 21:53:34 -0300

    Aulinha do for

---
+++ for.py
@@ -0,0 +1,32 @@
+# formas de se usar o for:
+# com um generator. Ex.: for n in range(10)
+# com um iterador. Ex.: for n in [1, 2, 3, 4, 5]
+
+# usando com iterator.
+# string = "juliana"
+
+# for c in string:
+#     print(c)
+
+# usando com generator.
+# for i in range(10):
+#     print(i)
+
+# Quando teremos que usar while?
+# Quando não sabemos o tamanho do iterável.
+# Neste caso irei ter que usar o while True.
+
+# Quando teremos que usar for?
+# Quando voce quiser iterar sobre uma lista ou string.
+# Porque?
+# lista = [2,7,6,4,5]
+
+# c = 0
+# while c < len(lista):
+#     print(lista[c])
+
+# for c in lista:
+#     print(c)
+
+# for c in range(5,10):
+#     print(c)
