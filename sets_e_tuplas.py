14f3236ec4bb21fa91c43183e78e33a4a648824f
Guilherme Xavier <guigo@eq.ufrj.br>
2023-09-28 20:34:46 -0300

    Adicionando arquivos da aula de sexta feira

---
+++ sets_e_tuplas.py
@@ -0,0 +1,16 @@
+# Sets:
+# lista = [1, 2, 3, 3, 4, 5, 5]
+# conjunto = {1, 2, 3, 3, 4, 5, 5}
+# conjunto2 = set(lista)
+
+# print(conjunto)
+
+# Tuplas:
+# lista = [1, 2, 3, 3, 4, 5, 5]
+# tupla = (1, 2)
+
+# um, dois = tupla
+
+# print(um)
+
+# print(tupla)
