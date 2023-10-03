692e914c1af58cc3dc9e9f030edd147bf745186e
Guigo1008 <guigo@eq.ufrj.br>
2023-10-03 15:55:06 -0300

    Upando funcoes.

---
+++ funcoes_1.py
@@ -0,0 +1,11 @@
+# O que sao funcoes?
+def substracao(y=10, x=10):
+    variavel_substracao = x - y
+    return variavel_substracao
+
+def ehpar(numero):
+    return numero % 2 == 0
+
+# print(ehpar(10))
+print(substracao())
+
